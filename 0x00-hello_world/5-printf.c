1b9ddb825fcca75d0fc628bb7e182276baf8b66f
suleohis <suleephraim1@gmail.com>
2022-09-08 19:48:47 +0100

    0x00. C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
  * main - entry point
  * Return: always returns a value
  */
-int main(void) {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return 0;
+int main(void) 
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
 }
