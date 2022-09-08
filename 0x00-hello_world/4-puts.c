6fa7c5084e2fe74d06a1a6962ff3e344eb6e915d
suleohis <suleephraim1@gmail.com>
2022-09-08 19:45:07 +0100

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
  * Return: always returns a value
  */
 int main(void) {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
