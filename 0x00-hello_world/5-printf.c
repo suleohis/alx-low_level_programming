3186c0956215c56b1c824630d818ab856d929936
suleohis <suleephraim1@gmail.com>
2022-09-08 20:04:35 +0100

    0x00. C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,7 @@
  */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammer, but the outcome is a piece of art,");
+printf("\n");
 return (0);
 }
