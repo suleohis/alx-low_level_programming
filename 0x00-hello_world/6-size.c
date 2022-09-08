a682cd1440c1afe9bb0ecb06936c6da50f17f658
suleohis <suleephraim1@gmail.com>
2022-09-08 17:55:21 +0100

    0x00. C - Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	char a;
+	int  i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+        printf("Size of an int: %zu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));
+	printf("Size of a float: %zu byte(s)\n", sizeof(f));
+
+	return 0;
+}
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * man -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	return 0;
+}	
