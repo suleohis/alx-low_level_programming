437968480c2f10a6d6b1a8e2a8011a069e5831a9
suleohis <suleephraim1@gmail.com>
2022-09-09 18:39:25 +0100

    0x00. C - Hello, World

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ else if( lastDigit == 0)
 }
 else 
 {
-	printf("Last digit of %d is %d and less than 6 and not 0\n", n, lastDigit)
+	printf("Last digit of %d is %d and less than 6 and not 0\n", n, lastDigit);
 }
 return (0);
 }
