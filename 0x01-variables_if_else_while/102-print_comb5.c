118e4b10fa9dc5a698663256ca97556de70e2690
suleohis <suleephraim1@gmail.com>
2022-09-09 23:04:40 +0100

    0x00. C - Hello, World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,27 +5,21 @@
  */
 int main(void)
 {
-int i, j, k, l;
-for (i = 0; i <= 9; i++)
+int num, i;
+for (num = 0; num <= 98; num++)
 {
-	for (j = 0; j <= 9; j++)
-	{
-		for (k = 0; k <= 9; k++)
-		{
-			for (l = k > 0 ? 0 : j > 0 || i > 0 ? 0 : 1; l <= 9; l++)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(' ');
-				putchar(k + '0');
-				putchar(l + '0');
-				if (i == 9 && j == 9 && k == 9 && l == 9)
-				continue;
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+for (i = num + 1; i <= 99; i++)
+{
+putchar((num / 10) + '0');
+putchar((num % 10) + '0');
+putchar(' ');
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+if (num == 98 && i == 99)
+continue;
+putchar(',');
+putchar(' ');
+}
 }
 putchar('\n');
 return (0);
