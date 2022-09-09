bdfd5355eb4bb2f84f16bead99a6db17efce7f92
suleohis <suleephraim1@gmail.com>
2022-09-09 18:18:40 +0100

    0x00. C - Hello, World

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ * Return: always return 0
+ *
+ */
 int main(void) {
 	int n;
 	srand(time(0));
