5994712061761f7e36aec68f1e48258f4a4a3ac6
suleohis <suleephraim1@gmail.com>
2022-09-16 03:32:02 +0100

    0x00. C - Nested Loop

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_number -  a function that prints the numbers, from 0 to 9.
+ * print_numbers -  a function that prints the numbers, from 0 to 9.
  */
 
 void print_numbers(void)
