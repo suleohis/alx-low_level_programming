1b52b0f04b969c975d0be3c3170d132de4402d65
suleohis <suleephraim1@gmail.com>
2022-09-19 22:15:30 +0100

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,12 +10,12 @@ void puts_half(char *str)
 {
 int i, len, n;
 len = strlen(str);
-if (len % 2 == 0)
-{
-	n = len / 2;
-} else
+if (len % 2 != 0)
 {
 	n = (len - 1) / 2;
+} else
+{
+	n = len / 2;
 }
 for (i = n; i < len; ++i)
 {
