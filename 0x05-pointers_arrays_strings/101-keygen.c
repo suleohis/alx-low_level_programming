e4d57e29dd979ebad4f606bd48364a42bee6e504
suleohis <suleephraim1@gmail.com>
2022-09-20 12:22:00 +0100

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,19 +3,17 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+int main(void)
 {
-    srand((unsigned int)(time(NULL)));
-    int i;
-    char pass[12];
+srand((unsigned int)(time(NULL)));
+int i;
+char pass[12];
 
-    printf("Press enter to get a twelve-character password\n");
-    getchar();
-
-    for(i = 0; i < 12; i++) {
-    pass[i] = 33 + rand() % 94;
+for(i = 0; i < 12; i++)
+{
+pass[i] = 33 + rand() % 94;
 }
 pass[i] = '\0';
 printf("%s\n",pass);
-    printf("\n\n");
+printf("\n\n");
 }
