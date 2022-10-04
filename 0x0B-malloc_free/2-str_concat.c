19d1979ce24edffb5bea7a49506bb123429b53d2
suleohis <suleephraim1@gmail.com>
2022-10-04 06:10:33 +0100

    0x0B. C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,24 @@
 #include "main.h"
-#include <stdlib.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to char type
+ *
+ * Return: an integer length of the argument
+ */
+int _strlen(char *s)
+{
+	int len, shift = 0;
+	
+	for (len = 0; *(s + shift);)
+	{
+		len++;
+		shift++;
+	}
+	
+	return (len);
+}
+
 
 /**
  * str_concat - concatenates to strings, s1 and s2.
@@ -49,25 +68,4 @@ char *str_concat(char *s1, char *s2)
 	*(ptc + m) = 0;
 
 	return (ptc);
-}
-
-
-
-/**
- * _strlen - returns the length of a string
- * @s: pointer to char type
- *
- * Return: an integer length of the argument
- */
-int _strlen(char *s)
-{
-	int len, shift = 0;
-
-	for (len = 0; *(s + shift);)
-	{
-		len++;
-		shift++;
-	}
-
-	return (len);
 }
