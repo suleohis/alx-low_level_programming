7125ae0290503bbc881d3e7368d25f4b942fc6c0
suleohis <suleephraim1@gmail.com>
2022-10-05 05:40:07 +0100

    0x0B. C - malloc, free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
+/**
+ * getLength - a function to get the length of a char
+ * @av: char pointer
+ * Return: the length of a char
+ */
 int getLength(char *av)
 {
 	int len, shift;
+
 	shift = 0;
+
 	for (len = 0; *(av + shift);)
 	{
 		len++;
@@ -34,21 +41,30 @@ for (i = 0; i < n; i++)
 return (dest);
 }
 
+/**
+ * argstostr - a function that concatenates all the arguments of your program.
+ * @ac: integer
+ * @av: pointer to pointer char
+ *
+ * Return: a pointer to a new string, or NULL if it fails
+ */
+
 char *argstostr(int ac, char **av)
 {
 	int i, len;
 	char *ptr, *_ptr;
-	if(ac == 0)
+
+	if (ac == 0)
 		return (NULL);
 	len = 0;
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		len += getLength(av[i]);
 	}
 
-	_ptr = ptr = (char *)malloc(len + ac -1);
+	_ptr = ptr = (char *) malloc(len + ac - 1);
 
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		_memcpy(_ptr, av[i], getLength(av[i]));
 		_ptr += getLength(av[i]) + 1;
