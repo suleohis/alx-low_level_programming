7612d0936e9611b63a17d170142ae30e3403c84e
suleohis <suleephraim1@gmail.com>
2022-10-12 20:50:19 +0100

    0x0F. C - Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,14 +6,14 @@
  * @size: size array
  * @cmp: function pointer
  *
- * Retrun: int
+ * Return: int
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!array || !cmp)
+	if (array == NULL || cmp == NULL)
 		return (-1);
 
 	if (size <= 0)
