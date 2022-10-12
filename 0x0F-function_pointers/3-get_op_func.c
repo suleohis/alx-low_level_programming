7cb27b4325ce1e414deed57727b31c354859eec8
suleohis <suleephraim1@gmail.com>
2022-10-12 11:17:30 +0100

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * get_op_func - selects the correct function to perform the operation
+ * @s: the operator passed to the function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_div},
+		{NULL, NULL},
+	};
+
+	int i;
+
+	for (i = 0; i < 5; i++)
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+	return (NULL);	
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,18 @@
+#ifndef _FUNCTION_POINTER_H
+#define _FUNCTION_POINTER_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int _putchar(char c);
+
+#endif
