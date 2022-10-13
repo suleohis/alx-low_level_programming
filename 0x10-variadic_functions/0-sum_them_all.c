1eccdfa82eaaac36d5a2082371757ddd3afc17e7
suleohis <suleephraim1@gmail.com>
2022-10-13 15:01:07 +0100

    0x0F. C - Function pointers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,23 +1,27 @@
-#include <stdarg.h>
+#include "variadic_functions.h"
+
 /**
- * sum_them_all - returns the sum of all parameters
- * @n: the numbers of arguments being passed through
- * Return: the sum of all parameters
+ * sum_them_all - function that returns the sum of all its parameters.
+ * @n: unsigned int n
+ *
+ * Return: return sum of aall its parameters
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int sum = 0, i;
+	va_list ap;
+	unsigned int i, sum;
 
 	if (n == 0)
 		return (0);
 
-	va_start(valist, n);
+	va_start(ap, n);
 
+	sum = 0;
 	for (i = 0; i < n; i++)
-		sum += va_arg(valist, int);
+		sum += va_arg(ap, int);
 
-	va_end(valist);
+	va_end(ap);
 
 	return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,20 +10,17 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	if (separator != NULL)
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
 	{
-		unsigned int i;
-		va_list ap;
-
-		va_start(ap, n);
-
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(ap, int));
-			if (separator != NULL && i < n - 1)
-				printf("%s", separator);
-		}
-		printf("\n");
-		va_end(ap);
+		printf("%d", va_arg(ap, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
 	}
+	printf("\n");
+	va_end(ap);
 }
