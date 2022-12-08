c9d1a242f6bba25b55d942c2527fccddece27bbe
suleohis <suleephraim1@gmail.com>
2022-12-08 20:09:34 +0100

    0x15-file_io

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - a function that printsi
+ * all the elements of a dlistint_t list.
+ * @h: struct dlistint_s
+ * Return: size_t
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t *new;
+	int i = 0;
+	while (h)
+	{
+		new = h->next;
+		printf("%d\n", h->n);
+		h = new;
+		i++;
+	}
+	return (i);
+}
